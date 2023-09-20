f4cc80ec2db1e6c2e32274037524753618bc9860
Lucas <a.lucasd@gmail.com>
2023-09-20 02:19:56 -0300

    prod

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\CreateProductRequest;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function create(Request $request) {
+        Product::create( [
+            'name' => $request->name,
+            'price' => $request->price
+        ] );
+
+        return json_encode("The product ". $request->name ." was saved");
+    }
+
+    public function index(Request $request) {
+        $products = Product::paginate($request->perPage, ['*'], 'page', $request->page);
+        return $products;
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'price'
+    ];
+
+    protected $appends = ['usd_price'];
+
+    public function getUsdPriceAttribute() {
+        return env("USD_CONV", 1)*$this->price;
+    }
 }
